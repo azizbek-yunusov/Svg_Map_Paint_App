c938cd06b2c0c1574d591a45a625a017cce887e7
azizbek-yunusov <azizbekaliyevich@gmail.com>
2025-01-04 10:54:16 +0500

    styled

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./i18n.ts";
+import '@mantine/core/styles.css';
 import { HelmetProvider } from "react-helmet-async";
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
